40a1e5cc62071108b6dc3867cf4b0fb9108e542f
James Hughes <hugjimbo77@gmail.com>
2018-01-14 23:17:01 +0000

    commitActivity.js file added & comparison data console logged for parcel repo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var port = 3000;
 var request = require('request');
 var config = require('./config');
-var rp = require('request-promise');
+var CommitActivity = require('./models/commitActivity');
 var request = require('request');
 var bodyParser = require('body-parser');
 var fs = require('file-system');
@@ -16,9 +16,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-var url = 'https://api.github.com/users/jameshughes7';
-var myMates = ['jonathanstidwillvf', 'benjaminsunderland', 'neroshan12', 'jameshughes7','racoll'];
 
+var owner = 'parcel-bundler';
+var repo = 'parcel';
+var url = `https://api.github.com/repos/${owner}/${repo}/stats/participation`;
+var commitActivity = new CommitActivity();
 
 var options = {
   url: url,
@@ -30,31 +32,30 @@ var options = {
   }
 };
 
+
 function callback(error, response, body) {
   if (!error && response.statusCode == 200) {
-    var info = JSON.parse(body);
-    // console.log(body);
-    // response.render("home");
-    console.log(info.name);
-    console.log(info.bio);
-    console.log(info.location);
+    var data = JSON.parse(body);
+    var winterMonthsFactor = `${commitActivity.winterCommitterFactor(data.all)}`;
+   console.log(`For the ${repo} repo: Developers commit ${Math.round(winterMonthsFactor * 100) / 100} more times than the rest of the year`);
   }
 }
+
 
 request(options, callback);
 
 //Routes
 
-// app.get('/results', function(req, res) {
-//   var query = req.query.search;
-//   var url = 'https://api.github.com/users/?s=' + query;
-//   request(url, function(error, response, body) {
-//     if(!error && response.statusVode == 200) {
-//       var data = JSON.parse(body);
-//       res.render("home"), {data: dat};
-//     }
-//   });
-// });
+app.get('/results', function(req, res) {
+  var query = req.query.search;
+  var url = 'https://api.github.com/users/?s=' + query;
+  request(url, function(error, response, body) {
+    if(!error && response.statusVode == 200) {
+      var data = JSON.parse(body);
+      res.render("home"), {data: data};
+    }
+  });
+});
 
 //Server listener
 app.listen(port, function() {
--- models/commitActivity.js
+++ models/commitActivity.js
@@ -1,7 +1,18 @@
-'use srict'
+'use strict'
 
-function CommitActivity(data) {
+function CommitActivity() {
 
 }
+
+CommitActivity.prototype.winterCommitterFactor = function(commitData) {
+  var restOfYearCommitArrayElements = commitData.slice(0,40); // 40
+  var winterMonthsCommitArrayElements = commitData.slice(39,52); // 12
+  return this.average(winterMonthsCommitArrayElements) / this.average(restOfYearCommitArrayElements);
+};
+
+CommitActivity.prototype.average = function(array) {
+  var sumOfMonthsArray = array.reduce((a, b) => a + b, 0);
+  return sumOfMonthsArray / array.length;
+};
 
 module.exports = CommitActivity;
